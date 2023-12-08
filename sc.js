1104c5d02a9b9fecd79ac301bcceb98ea676edbf
Ravindu Vithanage <vithanage@thesanmark.com>
2023-12-08 12:28:56 +0530

    add tostar button close

--- sc.js
+++ sc.js
@@ -1,5 +1,7 @@
 document.getElementById('email').addEventListener("input",checkEmaill);
 document.getElementById('password').addEventListener("input",checkPassword);
+
+
 function checkEmaill(event){
 
     let email=event.target.value;
@@ -9,6 +11,7 @@ function checkEmaill(event){
         validateEmail(email);
     }else
         emailError.classList.remove('hidden');
+    return email;
 }
 
 function validateEmail(email){
@@ -24,6 +27,7 @@ function validateEmail(email){
         emailError.innerHTML=""
         return email =emailRegex.test(email);
 }
+
 function checkPassword(event){
 
     let password=event.target.value;
@@ -34,6 +38,7 @@ function checkPassword(event){
     }else
         passwordError.classList.remove('hidden');
 }  
+
 function validatePassword(password){
     const passwordError=document.getElementById("password-error");
     var passwordRegex= /^[A-Za-z]\w{7,14}$/;
@@ -44,3 +49,57 @@ function validatePassword(password){
     passwordError.innerHTML=""
     return password =passwordRegex.test(password);
 }  
+function login(){
+
+    var email=document.getElementById("email").value;
+    var password=document.getElementById("password").value;
+    let data = "\r Username: " + email+ " \r\n " + "Password: " + password ;
+  
+    
+    if(email.trim() == "" || password.trim() == ""){
+        isEmpty(email,password);
+    } else {
+        if(validateEmail(email) == true && validatePassword(password)== true){
+           
+            const textToBLOB = new Blob([data], { type: "text/plain" });
+            const link = document.createElement("a");
+            link.download = 'form.txt';
+            link.href = window.URL.createObjectURL(textToBLOB);
+            link.style.display = "none";
+            document.body.appendChild(link);
+            link.click();
+            link.remove();
+            createToster();          
+        }
+    }
+}
+
+function isEmpty(email,password){
+
+    const emailError=document.getElementById("email-error");
+    const passwordError=document.getElementById("password-error");
+    
+    if(email ){
+        checkEmaill(email);
+    }else if(password){
+        checkPassword(password);
+    }else{
+        emailError.classList.remove('hidden');
+        passwordError.classList.remove('hidden');
+
+    }   
+}
+function createToster(){
+
+    const succseefullMessage=document.getElementById("success");
+    succseefullMessage.classList.remove('hidden');
+    setTimeout(function(){ 
+    succseefullMessage.classList.add('hidden'); 
+        
+    }, 30000);
+}
+document.getElementById('close').addEventListener("click",function(){
+    const succseefullMessage=document.getElementById("success");
+    succseefullMessage.classList.add('hidden');
+});
+
